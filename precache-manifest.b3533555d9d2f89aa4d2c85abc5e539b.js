05f1925e3343e37d43a4dc9059efdcf712fae78b
Stefano Maiocchi <stefanomaiocchi@yahoo.com>
2019-03-07 23:39:38 +0100

    Updates

---
+++ precache-manifest.b3533555d9d2f89aa4d2c85abc5e539b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "61feb52cb4ea7de8012148f2683a3717",
+    "url": "/reactsafari3/static/media/VAN.61feb52c.png"
+  },
+  {
+    "revision": "6fc1abd9d398cb49966c",
+    "url": "/reactsafari3/static/css/main.b04f9638.chunk.css"
+  },
+  {
+    "revision": "723bfb038e7130ea74d2",
+    "url": "/reactsafari3/static/js/runtime~main.723bfb03.js"
+  },
+  {
+    "revision": "7fd7534b5fee137c4548",
+    "url": "/reactsafari3/static/js/2.7fd7534b.chunk.js"
+  },
+  {
+    "revision": "ea517e8febee68ae63769a326b96a980",
+    "url": "/reactsafari3/static/media/ROOM2.ea517e8f.png"
+  },
+  {
+    "revision": "0f9bc2f72bbacab4c8290bd5550e97be",
+    "url": "/reactsafari3/static/media/GIRAFFA0.0f9bc2f7.jpg"
+  },
+  {
+    "revision": "6ec90c51835c6b3d5f7b165cb048337a",
+    "url": "/reactsafari3/static/media/GUIDE.6ec90c51.png"
+  },
+  {
+    "revision": "299bf6414da33eeb3f94cde4b153f9bb",
+    "url": "/reactsafari3/static/media/CAR.299bf641.png"
+  },
+  {
+    "revision": "852cae666129937459e17f1550d7db33",
+    "url": "/reactsafari3/static/media/JEEP.852cae66.png"
+  },
+  {
+    "revision": "6fc1abd9d398cb49966c",
+    "url": "/reactsafari3/static/js/main.6fc1abd9.chunk.js"
+  },
+  {
+    "revision": "57ce618a0bd530d9ca7b64c9cc1f341b",
+    "url": "/reactsafari3/static/media/TENT1.57ce618a.png"
+  },
+  {
+    "revision": "7d3952cbdc9b52edd1ac1509967ddd12",
+    "url": "/reactsafari3/static/media/TENT2.7d3952cb.png"
+  },
+  {
+    "revision": "6dbb459f49a6a9fc7b79c3d8fdde3604",
+    "url": "/reactsafari3/static/media/TENT3.6dbb459f.png"
+  },
+  {
+    "revision": "d439a40a51142f9fd63c02d4c0a9012a",
+    "url": "/reactsafari3/static/media/GHEPARDO0.d439a40a.jpg"
+  },
+  {
+    "revision": "45c4e8ffdc44a16a2736885801258ac4",
+    "url": "/reactsafari3/static/media/ANTILOPE0.45c4e8ff.jpg"
+  },
+  {
+    "revision": "fcc44b1c24e4869104dff23bb2230bd1",
+    "url": "/reactsafari3/static/media/LION0.fcc44b1c.jpeg"
+  },
+  {
+    "revision": "43ca4caa5132594ad607eba86d896858",
+    "url": "/reactsafari3/static/media/WILDBEAST0.43ca4caa.jpg"
+  },
+  {
+    "revision": "942e35e8de082cb5722cd35bb10e3705",
+    "url": "/reactsafari3/index.html"
+  }
+];
